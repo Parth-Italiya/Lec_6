cecee3e56a3e41b0d7cb53e68a4a3ea736e5e150
parth <parthitaliya93@gmail.com>
2023-11-23 19:08:24 +0530

    completed

---
+++ LEC6_1_1.C
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+ #include<conio.h>
+
+ void main(){
+
+int n;
+int i=1;
+clrscr();
+printf("enter num: ");
+scanf("%d",&n);
+while(i<=n){
+printf("%d\n",i);
+i++;
+}
+getch();
+}
